85c05e1aa425582c42d7ebd713f18f11abc25de8
Koushan <kooshanfilm@hotmail.com>
2015-11-12 10:16:10 -0800

    introduciton to Java

---
+++ intro.java
@@ -0,0 +1,18 @@
+/*Java is compiled program meaning:
+     we need to run compile to run the readable java code into computer readable code
+
+if you need to run this on console run the following:
+(javac == complile) and then then name of the file
+after run java name of the file without.Java
+*/
+
+import java.io.Console;
+ 
+public class intro {
+  
+    public static void main(String[] args) {
+        Console console = System.console();
+        // Welcome to the Introductions program!  Your code goes below here
+        console.printf("This is introduction");
+  }
+}
