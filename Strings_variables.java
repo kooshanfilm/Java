d493877992bd320b7454fdc04d71dd724117d72a
Koushan <kooshanfilm@hotmail.com>
2015-11-12 10:15:52 -0800

    String

---
+++ Strings_variables.java
@@ -0,0 +1,20 @@
+/*
+skipp secuence : \n
+
+String firstName = "James";
+%s which is format string is think about where do you 
+want to call that variable and you need to , at the end 
+
+*/
+
+import java.io.Console;
+public class Strings_variables {
+  
+    public static void main(String[] args) {
+        Console console = System.console();
+
+        String firstName = "James";
+        console.printf("This is introduction %s\n",firstName);
+        console.printf("%s this is learning new lang",firstName);
+  }
+}
