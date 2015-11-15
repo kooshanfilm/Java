99e4d5d3ead76b6f61e1d3e0eaf7c246d9529762
Koushan <kooshanfilm@hotmail.com>
2015-11-14 16:20:25 -0800

    new codes

---
+++ Java_bible.java
@@ -0,0 +1,18 @@
+Java_bible:
+
+--to print in to the console:
+Console console = System.console();
+console.print();
+
+--to exit from the console:
+System.console(0;
+
+if and else:
+	if (variable < x) {
+
+	}
+
+--Parsing String to integer
+String age1 = console.readLine("your number")
+int age2 = ParsingInt(age1);
+
---
+++ conditional_statement.java
@@ -0,0 +1,19 @@
+/*
+conditional statement
+
+*/
+import java.io.Console;
+public class conditional_statement 
+{
+    public static void main(String[] args) {
+        Console console = System.console();
+		int age = 12;
+		if (age < 13){
+			console.printf("you can use this software");
+			System.exit(0);
+		}
+		      
+
+}
+}
+
